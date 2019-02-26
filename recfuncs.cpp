01d3692cc64a7143da840098452a554e591ae6d5
Marcel Plakolb <marcel.plakolb@liwest.at>
2019-02-26 10:49:41 +0100

    implemented empty and created Seg-fault

---
+++ recfuncs.cpp
@@ -0,0 +1,32 @@
+#include "recfuncs.h"
+#include <stdlib.h>
+
+int calc_array_size(int num){
+  return 0;
+}
+
+int convert_to_binary(int num){
+  return 0;
+}
+
+int* create_array(int size){
+  int name [size];
+}
+
+int calc_factoral(int num){
+  if (num>1) {
+    return num*calc_factoral(num-1);
+  }
+  else{
+    return 1;
+  }
+}
+
+void calc_sums(int* nums, int size, int* out_arr, int* out_arr_bin){
+
+}
+
+CalculationResults perform_calculations(int* nums, int num){
+  CalculationResults temp;
+  return temp;
+}
--- recfuncs.h
+++ recfuncs.h
@@ -1,6 +1,18 @@
 #ifndef ___RECURSIVE_POTPOURRI
 #define ___RECURSIVE_POTPOURRI
 
+struct CalculationResults{
+  int no_of_sums;
+  int no_of_nums;
+  int* sums;
+  int* binary_sums;
+  int* num_factorials;
+};
 
-
+int calc_array_size(int num);
+int convert_to_binary(int num);
+int* create_array(int size);
+int calc_factorial(int num);
+void calc_sums(int* nums, int size, int* out_arr, int* out_arr_bin);
+CalculationResults perform_calculations(int* nums, int num);
 #endif
